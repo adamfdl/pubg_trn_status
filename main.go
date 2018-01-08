f8775777e3ae500d4a8cb7571271c1f8c3172cd5
Adam Fadhil <adamfdls@gmail.com>
2018-01-08 11:02:14 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"gopkg.in/mailgun/mailgun-go.v1"
+)
+
+type TRNResponse struct {
+	Error string  `json:"error"`
+	Code  float64 `json:"code"`
+}
+
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{})
+}
+
+func main() {
+	go heartBeat()
+
+	var input string
+	fmt.Scanln(&input)
+
+}
+
+func TRNAPI() (*TRNResponse, string) {
+	url := "https://api.pubgtracker.com/v2/profile/pc/adamms"
+
+	// Request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("TRN-API-Key", "17d76a22-907b-4ab0-84fe-dfbc34cebd0c")
+
+	// Response
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	trnResponse := &TRNResponse{}
+	json.Unmarshal(body, trnResponse)
+
+	return trnResponse, string(body)
+}
+
+func heartBeat() {
+	for range time.Tick(time.Second * 10) {
+		response, stringResponse := TRNAPI()
+		if response.Error != "" && response.Code == 3 {
+			logrus.Warn("code: ", response.Code)
+			logrus.Warn("error: ", response.Error)
+			fmt.Println("========================")
+		} else {
+			logrus.Info(stringResponse)
+			fmt.Println("========================")
+
+			mailMe()
+		}
+	}
+}
+
+func mailMe() {
+	var counter int
+
+	if counter == 0 {
+		mg := mailgun.NewMailgun("sandbox3fd66e607c004414a32485674ce9674f.mailgun.org", "key-81f9124c51b2a16192b5d546df0cae3b", "pubkey-27244492e5d0f52210d61f36ec18620c")
+		message := mg.NewMessage(
+			"no-reply@example.com",
+			"PUBG Tracker Network Bot Status",
+			"Go check your server now bitch",
+			"adamfdls@gmail.com")
+		mg.Send(message)
+	}
+
+	counter++
+}
